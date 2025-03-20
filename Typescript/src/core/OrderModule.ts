f9c29b9a173a7845ce14782ff68fcc35f1c84448
Jasper Steenhuis <j.steenhuis@wowww.nl>
2025-03-20 18:16:43 +0100

    SRP... toch?

--- Typescript/src/core/OrderModule.ts
+++ Typescript/src/core/OrderModule.ts
@@ -32,21 +32,19 @@ class Emailer {
         console.log(`Body:\n${email.body}`);
     }
 }
-
-export class OrderModule {
-    order(type: HardwareType, number: number): void {
-        // Validation
-        if (number < 1 || number > 30) {
-            throw new Error(`Order ${number} of type ${type} seems incorrect`);
-        }
-        
-        // Order hardware with API
-        const apiCaller = new APICaller();
-        const result = apiCaller.placeOrder(type, number);
-        if (!result) {
-            throw new Error("Order failed");
-        }
-        
+class EmailComposer {
+    static composeEmail(address: string, orderDetails: string, invoiceDetails: string, type: HardwareType): Email {
+            const email = new Email(
+                address,
+                "Ordermodule@example.com",
+                `Invoice ${type}`,
+                invoiceDetails
+            );
+            return email;
+    }
+}
+class PriceCalculator {
+    static calculatePrice(type: HardwareType, number: number): Number {
         // Calculate price
         let price = 0;
         switch (type) {
@@ -62,20 +60,42 @@ export class OrderModule {
             default:
                 throw new Error("Invalid hardware type");
         }
-        
-        // Compose and send email
-        const address = "itbusiness@example.com";
-        const orderDetails = `${number} of ${type}`;
-        const invoiceDetails = `Customer email: ${address}\nDetails: ${orderDetails}\nPrice: ${price}`;
-        const email = new Email(
-            address,
-            "Ordermodule@example.com",
-            `Invoice ${type}`,
-            invoiceDetails
-        );
-        Emailer.sendEmail(email);
-        
-        console.log("Order processed");
+        return price;
+    }
+}
+class NumberValidator {
+    static validateNumber(type: HardwareType, number: number): Boolean {
+        // Validation
+        if (number < 1 || number > 30) {
+            throw new Error(`Order ${number} of type ${type} seems incorrect`);
+            return false;
+        }
+        return true;
+    }
+}
+export class OrderModule {
+    order(type: HardwareType, number: number): void {
+
+        let valid = NumberValidator.validateNumber(type, number);
+
+        if(valid){
+            // Order hardware with API
+            const apiCaller = new APICaller();
+            const result = apiCaller.placeOrder(type, number);
+            if (!result) {
+                throw new Error("Order failed");
+            }
+
+            let price = PriceCalculator.calculatePrice(type, number);
+
+            // Compose and send email
+            const address = "itbusiness@example.com";
+            const orderDetails = `${number} of ${type}`;
+            const invoiceDetails = `Customer email: ${address}\nDetails: ${orderDetails}\nPrice: ${price}`;
+            Emailer.sendEmail(EmailComposer.composeEmail(address,orderDetails,invoiceDetails, type));
+
+            console.log("Order processed");
+        }
     }
 }
 
